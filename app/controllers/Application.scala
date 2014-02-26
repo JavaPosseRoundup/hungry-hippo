54de76b2b7208cddfd4d5d7e46ab9cde38a1807a
Julie Pitt <julie@julieonjava.com>
2014-02-26 10:45:25 -0800

    Added /start and /stop endpoints that call the crawl controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,26 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import akka.actor.{Props, ActorRef}
+import play.api.libs.concurrent.Akka
+import com.javaposse.hungryhippo.actors.{StopCrawling, StartCrawling, CrawlControlActor}
+import play.api.Play.current
 
 object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Hello Play Framework"))
   }
+
+  def startCrawler = Action {
+    val crawlControlActor: ActorRef = Akka.system.actorOf(Props[CrawlControlActor])
+    crawlControlActor ! StartCrawling
+    Ok("started")
+  }
+
+  def stopCrawler = Action {
+    val crawlControlActor: ActorRef = Akka.system.actorOf(Props[CrawlControlActor])
+    crawlControlActor ! StopCrawling
+    Ok("stopped")
+  }
+
 }
